a537e7b950afae802e01434a00e541d62da52eb0
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-02-20 14:58:55 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,13 +5,22 @@ import (
     "io"
     "log"
     "net"
+    "net/http"
     "os"
     "strings"
+    "sync"
 )
 var (
-    bind = flag.String("b", ":10000", "bind")
-    ipst = flag.String("i", "IPlist", "iplist")
-    tars = flag.String("t", "", "target")
+
+    addr = flag.String("a", ":1", "addr")
+    ibnd = flag.String("i", ":1001", "i")
+    obnd = flag.String("o", ":1000", "o")
+    path = flag.String("p", "./", "path")
+    mute = sync.Mutex{}
+
+    //bind = flag.String("b", ":10000", "bind")
+    //ipst = flag.String("i", "IPlist", "iplist")
+    //tars = flag.String("t", "", "target")
 )
 func main() {
     flag.Parse()
@@ -65,4 +74,43 @@ func inIPlist(ip string, iplist string) bool {
         log.Println("[ERR-6] ", err)
     }
     return false
+}
+
+
+
+package main
+import (
+
+)
+var (
+
+)
+func main() {
+    flag.Parse()
+    file, err := os.OpenFile("IPlist", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        log.Fatal("[ERR-0] ", err)
+    }
+    defer file.Close()
+    http.HandleFunc(*path, func(w http.ResponseWriter, r *http.Request) {
+        ip, _, err := net.SplitHostPort(r.RemoteAddr)
+        if err != nil {
+            log.Println("[ERR-1] ", err)
+            http.Error(w, "[ERR-1]", http.StatusInternalServerError)
+            return
+        }
+        if _, err := w.Write([]byte(ip+"\n")); err != nil {
+            log.Println("[ERR-2] ", err)
+            http.Error(w, "[ERR-2]", http.StatusInternalServerError)
+            return
+        }
+        mute.Lock()
+        defer mute.Unlock()
+        if _, err := file.WriteString(ip+"\n"); err != nil {
+            log.Println("[ERR-3] ", err)
+            http.Error(w, "[ERR-3]", http.StatusInternalServerError)
+            return
+        }
+    })
+    log.Fatal(http.ListenAndServe(*addr, nil))
 }

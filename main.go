1b905afac2b9a1685b7de27d4f64ab23bf478660
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-02-20 21:53:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
     "sync"
 )
 var (
-    addr = flag.String("a", ":90", "addr")
+    addr = flag.String("a", ":80", "addr")
     ibnd = flag.String("i", "", "inbound")
     obnd = flag.String("o", ":10", "outbound")
     path = flag.String("p", "/iplist", "path")
@@ -21,11 +21,11 @@ func main() {
     flag.Parse()
     _, portAddr, err := net.SplitHostPort(*addr)
     if err != nil {
-        log.Fatal("[ERR-00]", err)
+        log.Fatal("[ERR-00]")
     }
     _, portObnd, err := net.SplitHostPort(*obnd)
     if err != nil {
-        log.Fatal("[ERR-01]", err)
+        log.Fatal("[ERR-01]")
     }
     if portAddr == portObnd {
         log.Fatal("[ERR-10]")
@@ -44,26 +44,26 @@ func main() {
 func ListenAndAuth() {
     file, err := os.OpenFile("IPlist", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
     if err != nil {
-        log.Fatal("[ERR-11]", err)
+        log.Fatal("[ERR-11]")
     }
     defer file.Close()
     http.HandleFunc(*path, func(w http.ResponseWriter, r *http.Request) {
         ip, _, err := net.SplitHostPort(r.RemoteAddr)
         if err != nil {
-            log.Println("[ERR-12]", err)
-            http.Error(w, "[ERR-12]", http.StatusInternalServerError)
+            log.Println("[ERR-12]")
+            http.Error(w, "[ERR-12]", 500)
             return
         }
         if _, err := w.Write([]byte(ip+"\n")); err != nil {
-            log.Println("[ERR-13]", err)
-            http.Error(w, "[ERR-13]", http.StatusInternalServerError)
+            log.Println("[ERR-13]")
+            http.Error(w, "[ERR-13]", 500)
             return
         }
         mute.Lock()
         defer mute.Unlock()
         if _, err := file.WriteString(ip+"\n"); err != nil {
-            log.Println("[ERR-14]", err)
-            http.Error(w, "[ERR-14]", http.StatusInternalServerError)
+            log.Println("[ERR-14]")
+            http.Error(w, "[ERR-14]", 500)
             return
         }
     })
@@ -72,13 +72,13 @@ func ListenAndAuth() {
 func ListenAndCopy() {
     listener, err := net.Listen("tcp", *obnd)
     if err != nil {
-        log.Fatal("[ERR-21]", err)
+        log.Fatal("[ERR-21]")
     }
     defer listener.Close()
     for {
         outConn, err := listener.Accept()
         if err != nil {
-            log.Println("[WAR-22]", err)
+            log.Println("[WAR-22]")
             continue
         }
         go handleOut(outConn)
@@ -88,12 +88,12 @@ func handleOut(outConn net.Conn) {
     defer outConn.Close()
     clientIP := outConn.RemoteAddr().(*net.TCPAddr).IP.String()
     if !inIPlist(clientIP, "IPlist") {
-        log.Println("[WAR-23]", clientIP)
+        log.Println("[WAR-23]")
         return
     }
     inConn, err := net.Dial("tcp", *ibnd)
     if err != nil {
-        log.Println("[WAR-24]", err)
+        log.Println("[WAR-24]")
         return
     }
     defer inConn.Close()
@@ -103,7 +103,7 @@ func handleOut(outConn net.Conn) {
 func inIPlist(ip string, iplist string) bool {
     file, err := os.Open(iplist)
     if err != nil {
-        log.Println("[WAR-25]", err)
+        log.Println("[WAR-25]")
         return false
     }
     defer file.Close()
@@ -114,7 +114,7 @@ func inIPlist(ip string, iplist string) bool {
         }
     }
     if err := scanner.Err(); err != nil {
-        log.Println("[WAR-26]", err)
+        log.Println("[WAR-26]")
     }
     return false
 }

290075839a691a0fd91b0346857b74ddcabca878
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-02-22 07:10:16 +0800

    Remove TLS

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 import (
     "bufio"
-    "crypto/tls"
     "flag"
     "io"
     "log"
@@ -13,9 +12,7 @@ import (
 )
 var (
     addr = flag.String("a", ":90", "addr")
-    crtf = flag.String("c", "crt.pem", "")
     ibnd = flag.String("i", "", "inbound")
-    keyf = flag.String("k", "key.pem", "")
     obnd = flag.String("o", ":10", "outbound")
     path = flag.String("p", "/iplist", "path")
     mute = sync.Mutex{}
@@ -24,81 +21,64 @@ func main() {
     flag.Parse()
     _, portAddr, err := net.SplitHostPort(*addr)
     if err != nil {
-        log.Fatal("[ERR-00]", err)
+        log.Fatal("[ERR-00] ", err)
     }
     _, portObnd, err := net.SplitHostPort(*obnd)
     if err != nil {
-        log.Fatal("[ERR-01]", err)
+        log.Fatal("[ERR-01] ", err)
     }
     if portAddr != portObnd {
         log.Printf("[LISTEN] %v%v\n", *addr, *path)
         go ListenAndAuth()
     } else {
-        log.Fatal("[ERR-03]", "Server Port Conflict")
+        log.Fatal("[ERR-02] ", "Server Port Conflict")
     }
     if *ibnd != "" {
         log.Printf("[LISTEN] %v <-> %v\n", *obnd, *ibnd)
         go ListenAndCopy()
     } else {
-        log.Println("[WAR-00]", "No Inbound Service")
+        log.Println("[WAR-00] ", "No Inbound Service")
     }
     select {}
 }
 func ListenAndAuth() {
     file, err := os.OpenFile("IPlist", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
     if err != nil {
-        log.Fatal("[ERR-10]", err)
+        log.Fatal("[ERR-10] ", err)
     }
     defer file.Close()
     http.HandleFunc(*path, func(w http.ResponseWriter, r *http.Request) {
         ip, _, err := net.SplitHostPort(r.RemoteAddr)
         if err != nil {
-            log.Println("[ERR-11]", err)
+            log.Println("[ERR-11] ", err)
             http.Error(w, "[ERR-11]", 500)
             return
         }
         if _, err := w.Write([]byte(ip+"\n")); err != nil {
-            log.Println("[ERR-12]", err)
+            log.Println("[ERR-12] ", err)
             http.Error(w, "[ERR-12]", 500)
             return
         }
         mute.Lock()
         defer mute.Unlock()
         if _, err := file.WriteString(ip+"\n"); err != nil {
-            log.Println("[ERR-13]", err)
+            log.Println("[ERR-13] ", err)
             http.Error(w, "[ERR-13]", 500)
             return
         }
     })
-    if _, err := tls.LoadX509KeyPair(*crtf, *keyf); err!=nil {
-        log.Println("[WAR-10]", err)
-        log.Fatal(http.ListenAndServe(*addr, nil))
-    } else {
-        log.Fatal(http.ListenAndServeTLS(*addr, *crtf, *keyf, nil))
-    }
+    log.Fatal(http.ListenAndServe(*addr, nil))
 }
 func ListenAndCopy() {
-    var listener net.Listener
-    if _, err := tls.LoadX509KeyPair(*crtf, *keyf); err!=nil {
-        log.Println("[WAR-20]", err)
-        listener, err := net.Listen("tcp", *obnd)
-        if err != nil {
-            log.Println("[WAR-21]", err)
-        } else {
-            cert, _ := tls.LoadX509KeyPair(*crtf, *keyf)
-            listener, err := tls.Listen("tcp", *obnd, &tls.Config{
-                Certificates: []tls.Certificate{cert},
-            })
-            if err != nil {
-                log.Fatal("[ERR-20]", err)
-            }
-        }
+    listener, err := net.Listen("tcp", *obnd)
+    if err != nil {
+        log.Println("[WAR-20] ", err)
     }
     defer listener.Close()
     for {
         outConn, err := listener.Accept()
         if err != nil {
-            log.Println("[WAR-22]", err)
+            log.Println("[WAR-21] ", err)
             continue
         }
         go handleOut(outConn)
@@ -108,17 +88,17 @@ func handleOut(outConn net.Conn) {
     defer outConn.Close()
     clientIP := outConn.RemoteAddr().(*net.TCPAddr).IP.String()
     if !inIPlist(clientIP, "IPlist") {
-        log.Println("[WAR-30]", clientIP)
+        log.Println("[WAR-30] ", clientIP)
         return
     }
     inConn, err := net.Dial("tcp", *ibnd)
     if err != nil {
-        log.Println("[WAR-31]", err)
+        log.Println("[WAR-31] ", err)
         inConn, err = tls.Dial("tcp", *ibnd, &tls.Config{
             InsecureSkipVerify: true,
         })
         if err != nil {
-            log.Println("[WAR-32]", err)
+            log.Println("[WAR-32] ", err)
             return
         }
     }
@@ -129,7 +109,7 @@ func handleOut(outConn net.Conn) {
 func inIPlist(ip string, iplist string) bool {
     file, err := os.Open(iplist)
     if err != nil {
-        log.Println("[WAR-40]", err)
+        log.Println("[WAR-40] ", err)
         return false
     }
     defer file.Close()
@@ -140,7 +120,7 @@ func inIPlist(ip string, iplist string) bool {
         }
     }
     if err := scanner.Err(); err != nil {
-        log.Println("[WAR-41]", err)
+        log.Println("[WAR-41] ", err)
     }
     return false
 }

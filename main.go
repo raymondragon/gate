6b29ce98c3f6b45c663a75bb7a162adc9a366800
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-03-05 11:36:03 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 import (
     "bufio"
+    "crypto/rand"
+    "crypto/rsa"
+    "crypto/tls"
+    "crypto/x509"
+    "crypto/x509/pkix"
+    "encoding/pem"
     "flag"
     "io"
     "log"
+    "math/big"
     "net"
     "net/http"
     "net/url"
     "os"
     "strings"
+    "time"
 )
 var (
     authURL = flag.String("A", "", "Authentication")
@@ -57,38 +65,54 @@ func main() {
     }
 }
 func ListenAndAuth(parsedURL ParsedURL) {
+    httpHandler := func(w http.ResponseWriter, r *http.Request) {
+        clientIP, _, err := net.SplitHostPort(r.RemoteAddr)
+        if err != nil {
+            log.Printf("[WARN-0] %v", err)
+            return
+        }
+        if _, err := w.Write([]byte(clientIP + "\n")); err != nil {
+            log.Printf("[WARN-1] %v", err)
+            http.Error(w, "[WARN-1]", 500)
+            return
+        }
+        file, err := os.OpenFile("IPlist", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+        if err != nil {
+            log.Printf("[WARN-2] %v", err)
+            return
+        }
+        defer file.Close()
+        if inIPlist(clientIP, "IPlist") {
+            return
+        }
+        if _, err := file.WriteString(clientIP + "\n"); err != nil {
+            log.Printf("[WARN-3] %v", err)
+            return
+        }
+    }
     switch parsedURL.Scheme {
     case "http":
-        http.HandleFunc(parsedURL.Path, func(w http.ResponseWriter, r *http.Request) {
-            clientIP, _, err := net.SplitHostPort(r.RemoteAddr)
-            if err != nil {
-                log.Printf("[WARN-0] %v", err)
-                return
-            }
-            if _, err := w.Write([]byte(clientIP + "\n")); err != nil {
-                log.Printf("[WARN-1] %v", err)
-                http.Error(w, "[WARN-1]", 500)
-                return
-            }
-            file, err := os.OpenFile("IPlist", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-            if err != nil {
-                log.Printf("[WARN-2] %v", err)
-                return
-            }
-            defer file.Close()
-            if inIPlist(clientIP, "IPlist") {
-                return
-            }
-            if _, err := file.WriteString(clientIP + "\n"); err != nil {
-                log.Printf("[WARN-3] %v", err)
-                return
-            }
-        })
-        if err := http.ListenAndServe(parsedURL.Hostname+":"+ parsedURL.Port, nil); err != nil {
+        http.HandleFunc(parsedURL.Path, httpHandler)
+        if err := http.ListenAndServe(parsedURL.Hostname+":"+parsedURL.Port, nil); err != nil {
             log.Fatalf("[ERRO-5] %v", err)
         }
+    case "https":
+        cert, err := generateCert(parsedURL.Hostname)
+        if err != nil {
+            log.Fatalf("[ERRO-6] %v", err)
+        }
+        server := &http.Server{
+            Addr:      parsedURL.Hostname+":"+parsedURL.Port,
+            TLSConfig: &tls.Config{
+                Certificates: []tls.Certificate{cert},
+            },
+        }
+        http.HandleFunc(parsedURL.Path, httpHandler)
+        if err := server.ListenAndServeTLS("", ""); err != nil {
+            log.Fatalf("[ERRO-7] %v", err)
+        }
     default:
-        log.Fatalf("[ERRO-6] %v", "URL Scheme Unsupported")
+        log.Fatalf("[ERRO-8] %v", "URL Scheme Unsupported")
     }
 }
 func ListenAndCopy(parsedURL ParsedURL, authEnabled bool) {
@@ -96,7 +120,7 @@ func ListenAndCopy(parsedURL ParsedURL, authEnabled bool) {
     case "tcp":
         listener, err := net.Listen("tcp", parsedURL.Hostname+":"+parsedURL.Port)
         if err != nil {
-            log.Fatalf("[ERRO-7] %v", err)
+            log.Fatalf("[ERRO-9] %v", err)
         }
         defer listener.Close()
         for {
@@ -114,7 +138,7 @@ func ListenAndCopy(parsedURL ParsedURL, authEnabled bool) {
                 }
                 remoteConn, err := net.Dial("tcp", parsedURL.Fragment)
                 if err != nil {
-                    log.Fatalf("[ERRO-8] %v", err)
+                    log.Fatalf("[ERRO-A] %v", err)
                 }
                 defer remoteConn.Close()
                 go io.Copy(remoteConn, localConn)
@@ -122,7 +146,7 @@ func ListenAndCopy(parsedURL ParsedURL, authEnabled bool) {
             }(localConn)
         }
     default:
-        log.Fatalf("[ERRO-9] %v", "URL Scheme Unsupported")
+        log.Fatalf("[ERRO-B] %v", "URL Scheme Unsupported")
     }
 }
 func urlParse(rawURL string) (ParsedURL, error) {
@@ -151,4 +175,31 @@ func inIPlist(ip string, list string) bool {
         }
     }
     return false
+}
+func generateCert(orgName string) (tls.Certificate, error) {
+    priv, err := rsa.GenerateKey(rand.Reader, 2048)
+    if err != nil {
+        return tls.Certificate{}, err
+    }
+    serialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
+    if err != nil {
+        return tls.Certificate{}, err
+    }
+    template := x509.Certificate{
+        SerialNumber: serialNumber,
+        Subject:      pkix.Name{
+            Organization: []string{orgName},
+        },
+        NotBefore:    time.Now(),
+        NotAfter:     time.Now().Add(10 * 365 * 24 * time.Hour),
+        KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+        ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+    }
+    crtDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+    if err != nil {
+        return tls.Certificate{}, err
+    }
+    crtPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: crtDER})
+    keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+    return tls.X509KeyPair(crtPEM, keyPEM)
 }

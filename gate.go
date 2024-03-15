d6b4de5b45726e070ce5c3d30bdb6ad4b4e5f34f
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-03-15 16:08:49 +0800

    Update gate.go

---
+++ gate.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+    "flag"
+    "io"
+    "log"
+    "net"
+    "net/http"
+    "os"
+    "strings"
+
+    "github.com/raymondragon/golib"
+)
+
+var (
+    authURL = flag.String("A", "", "Authorization: http(s)://local:port/secret_path#ipfile")
+    tranURL = flag.String("T", "", "Transmission: tcp(udp)://local:port/remote:port#ipfile")
+)
+
+func main() {
+    flag.Parse()
+    switch {
+    case *authURL != "" && *tranURL != "":
+        aURL, err := urlParse(*authURL)
+        if err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+        tURL, err := urlParse(*tranURL)
+        if err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+        if aURL.Fragment == "" {
+            aURL.Fragment = "IPlist"
+        }
+        tURL.Fragment = aURL.Fragment
+        log.Printf("[INFO] %v://%v:%v%v <-> [FILE] %v", aURL.Scheme, aURL.Hostname, aURL.Port, aURL.Path, aURL.Fragment)
+        go listenAndAuth(aURL)
+        log.Printf("[INFO] %v://%v:%v <-> %v", tURL.Scheme, tURL.Hostname, tURL.Port, strings.TrimPrefix(tURL.Path, "/"))
+        listenAndCopy(tURL, true)
+        select {}
+    case *authURL != "" && *tranURL == "":
+        aURL, err := urlParse(*authURL)
+        if err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+        log.Printf("[INFO] %v://%v:%v%v <-> [FILE] %v", aURL.Scheme, aURL.Hostname, aURL.Port, aURL.Path, aURL.Fragment)
+        listenAndAuth(aURL)
+    case *authURL == "" && *tranURL != "":
+        tURL, err := urlParse(*tranURL)
+        if err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+        if tURL.Fragment == "" {
+            log.Printf("[INFO] %v://%v:%v <-> %v", tURL.Scheme, tURL.Hostname, tURL.Port, strings.TrimPrefix(tURL.Path, "/"))
+            listenAndCopy(tURL, false)
+        } else {
+            log.Printf("[INFO] %v://%v:%v <-> %v [FILE] %v", tURL.Scheme, tURL.Hostname, tURL.Port, strings.TrimPrefix(tURL.Path, "/"), tURL.Fragment)
+            listenAndCopy(tURL, true)
+        }
+    default:
+        log.Fatalf("[ERRO] %v", "URL Flag Unprovided")
+    }
+}
+
+func listenAndAuth(parsedURL ParsedURL) {
+    http.HandleFunc(parsedURL.Path, func(w http.ResponseWriter, r *http.Request) {
+        golib.IPDisplayHandler(w, r)
+        golib.IPRecordHandler(parsedURL.Fragment)(w, r)
+    })
+    switch parsedURL.Scheme {
+    case "http":
+        log.Printf("[INFO] %v", *authURL)
+        if err := golib.ServeHTTP(parsedURL.Hostname, parsedURL.Port, nil); err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+    case "https":
+        tlsConfig, err := golib.TLSConfigApplication(parsedURL.Hostname)
+        if err != nil {
+            log.Printf("[WARN] %v", err)
+            tlsConfig, err = golib.TLSConfigGeneration(parsedURL.Hostname)
+            if err != nil {
+                log.Printf("[WARN] %v", err)
+            }
+        }
+        log.Printf("[INFO] %v", *authURL)
+        if err := golib.ServeHTTPS(parsedURL.Hostname, parsedURL.Port, nil, tlsConfig); err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+    default:
+        log.Fatalf("[ERRO] %v", parsedURL.Scheme)
+    }
+}
+
+func listenAndCopy(parsedURL ParsedURL, authEnabled bool) {
+    switch parsedURL.Scheme {
+    case "tcp":
+        localAddr, err := net.ResolveTCPAddr("tcp", parsedURL.Hostname+":"+parsedURL.Port)
+        if err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+        listener, err := net.ListenTCP("tcp", localAddr)
+        if err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+        defer listener.Close()
+        for {
+            localConn, err := listener.Accept()
+            if err != nil {
+                log.Printf("[WARN] %v", err)
+                continue
+            }
+            go func(localConn net.Conn) {
+                defer localConn.Close()
+                clientIP := localConn.RemoteAddr().(*net.TCPAddr).IP.String()
+                if authEnabled && !golib.IsInFile(clientIP, parsedURL.Fragment) {
+                    log.Printf("[WARN] %v", clientIP)
+                    return
+                }
+                remoteConn, err := net.Dial("tcp", strings.TrimPrefix(parsedURL.Path, "/"))
+                if err != nil {
+                    log.Fatalf("[ERRO] %v", err)
+                }
+                defer remoteConn.Close()
+                go io.Copy(remoteConn, localConn)
+                io.Copy(localConn, remoteConn)
+            }(localConn)
+        }
+    case "udp":
+        localAddr, err := net.ResolveUDPAddr("udp", parsedURL.Hostname+":"+parsedURL.Port)
+        if err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+        conn, err := net.ListenUDP("udp", localAddr)
+        if err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+        defer conn.Close()
+        for {
+            buf := make([]byte, 1024)
+            n, clientAddr, err := conn.ReadFromUDP(buf)
+            if err != nil {
+                log.Printf("[WARN] %v", err)
+                continue
+            }
+            clientIP := clientAddr.IP.String()
+            if authEnabled && !golib.IsInFile(clientIP, parsedURL.Fragment) {
+                log.Printf("[WARN] %v", clientIP)
+                continue
+            }
+            remoteAddr, err := net.ResolveUDPAddr("udp", strings.TrimPrefix(parsedURL.Path, "/"))
+            if err != nil {
+                log.Fatalf("[ERRO] %v", err)
+            }
+            if _, err := conn.WriteToUDP(buf[:n], remoteAddr); err != nil {
+                log.Printf("[WARN] %v", err)
+            }
+        }
+    default:
+        log.Fatalf("[ERRO] %v", parsedURL.Scheme)
+    }
+}
